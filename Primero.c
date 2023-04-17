6f5f0b3f374bc031fbadccf10b24104820d2b351
MathiasVeraM <mathias.vera@udla.edu.ec>
2023-04-17 16:29:26 -0500

    Segunda version

--- Primero.c
+++ Primero.c
@@ -1,5 +1,7 @@
+//hacer un hola mundo para entender git
 #include <stdio.h>
 int main(){
-    printf("Hola mundo");
+    printf("Hola mundo\n");
+    printf("Saludos gente hermosa");
     return 0;
 }
